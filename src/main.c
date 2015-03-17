8e804db6e1ba554615ceb99ca64a5afbb892d270
ItsEddy <itseddy0402@gmail.com>
2015-03-17 20:43:51 +0800

    Syntax tweak
    main.c: Provide USER_NAME macro a default value, so it is not required to be
    defined when compiling.
    Also unified the indent style.

--- src/main.c
+++ src/main.c
@@ -22,7 +22,10 @@
  */
 extern const unsigned char _sromfs;
 
-//static void setup_hardware();
+/* The default name, the actual name is define in makefile use -DUSER_NAME*/
+#ifndef USER_NAME
+#define USER_NAME user
+#endif
 
 volatile xSemaphoreHandle serial_tx_wait_sem = NULL;
 /* Add for serial input */
@@ -32,80 +35,79 @@ volatile xQueueHandle serial_rx_queue = NULL;
  * interrupts). */
 void USART2_IRQHandler()
 {
-	static signed portBASE_TYPE xHigherPriorityTaskWoken;
+    static signed portBASE_TYPE xHigherPriorityTaskWoken;
 
-	/* If this interrupt is for a transmit... */
-	if (USART_GetITStatus(USART2, USART_IT_TXE) != RESET) {
-		/* "give" the serial_tx_wait_sem semaphore to notfiy processes
-		 * that the buffer has a spot free for the next byte.
-		 */
-		xSemaphoreGiveFromISR(serial_tx_wait_sem, &xHigherPriorityTaskWoken);
+    /* If this interrupt is for a transmit... */
+    if (USART_GetITStatus(USART2, USART_IT_TXE) != RESET) {
+        /* "give" the serial_tx_wait_sem semaphore to notfiy processes
+         * that the buffer has a spot free for the next byte.
+         */
+        xSemaphoreGiveFromISR(serial_tx_wait_sem, &xHigherPriorityTaskWoken);
 
-		/* Diables the transmit interrupt. */
-		USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
-		/* If this interrupt is for a receive... */
-	}else if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
-		char msg = USART_ReceiveData(USART2);
+        /* Diables the transmit interrupt. */
+        USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
+        /* If this interrupt is for a receive... */
+    }else if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
+        char msg = USART_ReceiveData(USART2);
 
-		/* If there is an error when queueing the received byte, freeze! */
-		if(!xQueueSendToBackFromISR(serial_rx_queue, &msg, &xHigherPriorityTaskWoken))
-			while(1);
-	}
-	else {
-		/* Only transmit and receive interrupts should be enabled.
-		 * If this is another type of interrupt, freeze.
-		 */
-		while(1);
-	}
+        /* If there is an error when queueing the received byte, freeze! */
+        if(!xQueueSendToBackFromISR(serial_rx_queue, &msg, &xHigherPriorityTaskWoken))
+            while(1);
+    }
+    else {
+        /* Only transmit and receive interrupts should be enabled.
+         * If this is another type of interrupt, freeze.
+         */
+        while(1);
+    }
 
-	if (xHigherPriorityTaskWoken) {
-		taskYIELD();
-	}
+    if (xHigherPriorityTaskWoken) {
+        taskYIELD();
+    }
 }
 
 void send_byte(char ch)
 {
-	/* Wait until the RS232 port can receive another byte (this semaphore
-	 * is "given" by the RS232 port interrupt when the buffer has room for
-	 * another byte.
-	 */
-	while (!xSemaphoreTake(serial_tx_wait_sem, portMAX_DELAY));
+    /* Wait until the RS232 port can receive another byte (this semaphore
+     * is "given" by the RS232 port interrupt when the buffer has room for
+     * another byte.
+     */
+    while (!xSemaphoreTake(serial_tx_wait_sem, portMAX_DELAY));
 
-	/* Send the byte and enable the transmit interrupt (it is disabled by
-	 * the interrupt).
-	 */
-	USART_SendData(USART2, ch);
-	USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
+    /* Send the byte and enable the transmit interrupt (it is disabled by
+     * the interrupt).
+     */
+    USART_SendData(USART2, ch);
+    USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
 }
 
 char recv_byte()
 {
-	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
-	char msg;
-	while(!xQueueReceive(serial_rx_queue, &msg, portMAX_DELAY));
-	return msg;
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+    char msg;
+    while(!xQueueReceive(serial_rx_queue, &msg, portMAX_DELAY));
+    return msg;
 }
 void command_prompt(void *pvParameters)
 {
-	char buf[128];
-	char *argv[20];
+    char buf[128];
+    char *argv[20];
     char hint[] = USER_NAME "@" USER_NAME "-STM32:~$ ";
 
-	fio_printf(1, "\rWelcome to FreeRTOS Shell\r\n");
-	while(1){
-                fio_printf(1, "%s", hint);
-		fio_read(0, buf, 127);
-	
-		int n=parse_command(buf, argv);
+    fio_printf(1, "\rWelcome to FreeRTOS Shell\r\n");
+    while(1){
+        fio_printf(1, "%s", hint);
+        fio_read(0, buf, 127);
 
-		/* will return pointer to the command function */
-		cmdfunc *fptr=do_command(argv[0]);
-		if(fptr!=NULL)
-			fptr(n, argv);
-		else
-			fio_printf(2, "\r\n\"%s\" command not found.\r\n", argv[0]);
-	}
+        int n=parse_command(buf, argv);
 
+        /* will return pointer to the command function */
+        cmdfunc *fptr=do_command(argv[0]);
+        if(fptr!=NULL)
+            fptr(n, argv);
+        else
+            fio_printf(2, "\r\n\"%s\" command not found.\r\n", argv[0]);
+    }
 }
 
 void system_logger(void *pvParameters)
@@ -140,48 +142,46 @@ void system_logger(void *pvParameters)
             host_action(SYS_CLOSE, handle);
             return;
         }
-
         vTaskDelay(xDelay);
     }
-    
     host_action(SYS_CLOSE, handle);
 }
 
 int main()
 {
-	init_rs232();
-	enable_rs232_interrupts();
-	enable_rs232();
-	
-	fs_init();
-	fio_init();
-	
-	register_romfs("romfs", &_sromfs);
-	
-	/* Create the queue used by the serial task.  Messages for write to
-	 * the RS232. */
-	vSemaphoreCreateBinary(serial_tx_wait_sem);
-	/* Add for serial input 
-	 * Reference: www.freertos.org/a00116.html */
-	serial_rx_queue = xQueueCreate(1, sizeof(char));
+    init_rs232();
+    enable_rs232_interrupts();
+    enable_rs232();
+
+    fs_init();
+    fio_init();
+
+    register_romfs("romfs", &_sromfs);
+
+    /* Create the queue used by the serial task.  Messages for write to
+     * the RS232. */
+    vSemaphoreCreateBinary(serial_tx_wait_sem);
+    /* Add for serial input 
+     * Reference: www.freertos.org/a00116.html */
+    serial_rx_queue = xQueueCreate(1, sizeof(char));
 
     register_devfs();
-	/* Create a task to output text read from romfs. */
-	xTaskCreate(command_prompt,
-	            (signed portCHAR *) "CLI",
-	            512 /* stack size */, NULL, tskIDLE_PRIORITY + 2, NULL);
+    /* Create a task to output text read from romfs. */
+    xTaskCreate(command_prompt,
+            (signed portCHAR *) "CLI",
+            512 /* stack size */, NULL, tskIDLE_PRIORITY + 2, NULL);
 
 #if 0
-	/* Create a task to record system log. */
-	xTaskCreate(system_logger,
-	            (signed portCHAR *) "Logger",
-	            1024 /* stack size */, NULL, tskIDLE_PRIORITY + 1, NULL);
+    /* Create a task to record system log. */
+    xTaskCreate(system_logger,
+            (signed portCHAR *) "Logger",
+            1024 /* stack size */, NULL, tskIDLE_PRIORITY + 1, NULL);
 #endif
 
-	/* Start running the tasks. */
-	vTaskStartScheduler();
+    /* Start running the tasks. */
+    vTaskStartScheduler();
 
-	return 0;
+    return 0;
 }
 
 void vApplicationTickHook()
